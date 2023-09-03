2601d5777f722e2563ab12b634539253571ef804
居正中 <AlanJui.1960@gmail.com>
2023-09-03 11:09:15 +0800

    調整：(1) 選用之 color-scheme； (2) 統一 hightlights 在 Comment 的 用色為： #999999 。

---
+++ highlights/tokyonight.lua
@@ -0,0 +1,6 @@
+return { -- this table overrides highlights in all themes
+  Normal = { bg = "#000000" },
+  -- FILE_NAME = { fg = "#FFFFFF" },
+  -- Fix comment too dark and hard to read
+  Comment = { fg = "#999999" },
+}
